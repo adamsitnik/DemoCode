8387d0dcc5a6ba4bb5d6064c14b121f4d1bae93a
Adam Sitnik <adam.sitnik@gmail.com>
2015-11-05 16:54:37 +0100

    Exception in finally block

---
+++ Exceptions/Exceptional/Demos/ExceptionInFinallyBlock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Exceptional.Demos
+{
+    class ExceptionInFinallyBlock : Demoable
+    {
+        protected override string Description => "What happens if a finally block throws an exception?";
+
+        protected override void RunDemo()
+        {
+            try
+            {
+                try
+                {
+                    throw new Exception("Initial");
+                }
+                finally
+                {
+                    throw new Exception("thrown in finally block");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debugger.Break();
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
