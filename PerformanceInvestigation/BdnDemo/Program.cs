9c32168d397fa6d990db708a3e7d79b720c69b0b
Adam Sitnik <adam.sitnik@gmail.com>
2020-06-12 12:33:25 +0200

    add BDN demo

---
+++ PerformanceInvestigation/BdnDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using HardwareIntrinsics.RayTracer;
+
+namespace BdnDemo
+{
+    class Program
+    {
+        static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+    }
+
+    public class Benchmarks
+    {
+        SoA _soa = new SoA();
+
+        [Benchmark]
+        public void Render() => _soa.Render();
+    }
+}
